50b48843023b4a063e18c93a32f4bf9ba7977344
dimfot3 <fotiou.dimitris3@gmail.com>
2021-11-08 01:40:10 +0200

    Added gitignore and tests for sequential implementation

--- code/src/sequential_masked_implementation.c
+++ code/src/sequential_masked_implementation.c
@@ -12,12 +12,12 @@ float triagle_counting_sequential_masked_implementation(struct CSR_mtx *mtx, flo
     int counter = 0;
     for(int i = 0; i < mtx->mat_size; i++)          //all rows      
     {
-        for(int j = mtx->row_idx[i]; j < mtx->row_idx[i+1] && mtx->col_idx[j] < i; j++)    // all non zero colums of i row. this is the actual mask
+        for(int j = mtx->row_idx[i]; j < mtx->row_idx[i+1]; j++)    // all non zero colums of i row. this is the actual mask
         {
             int col = mtx->col_idx[j];
-            for(int k = mtx->row_idx[col]; k < mtx->row_idx[col+1] && mtx->col_idx[k] < col; k++)        // all non zero columns of row[col]
+            for(int k = mtx->row_idx[col]; k < mtx->row_idx[col+1]; k++)        // all non zero columns of row[col]
             {
-                for(int l = mtx->row_idx[i]; l < mtx->row_idx[i+1] && mtx->col_idx[l] < col; l++)           //all the non zero colums in i row
+                for(int l = mtx->row_idx[i]; l < mtx->row_idx[i+1]; l++)           //all the non zero colums in i row
                 {
                     int col1 = mtx->col_idx[l];
                     int col2 = mtx->col_idx[k];
--- code/tests/src/seq_implementation_test.cpp
+++ code/tests/src/seq_implementation_test.cpp
@@ -8,7 +8,7 @@ extern "C" {
 TEST(sequential, t1) {
   struct COO_mtx *mtx_coo_fmt = (COO_mtx*) malloc(sizeof(struct COO_mtx));
   struct CSR_mtx *mtx_csr_fmt = (CSR_mtx*) malloc(sizeof(struct CSR_mtx));
-  read_matrix((char**)"../data/test.mtx", mtx_coo_fmt, 0);
+  read_matrix((char**)"../data/test1.mtx", mtx_coo_fmt, 0);
   coo_to_csr(mtx_coo_fmt, mtx_csr_fmt);
   float time_taken;
   float num = triagle_counting_sequential_masked_implementation(mtx_csr_fmt,&time_taken,0);
@@ -19,12 +19,13 @@ TEST(sequential, t1) {
 TEST(sequential, t2) {
   struct COO_mtx *mtx_coo_fmt = (COO_mtx*) malloc(sizeof(struct COO_mtx));
   struct CSR_mtx *mtx_csr_fmt = (CSR_mtx*) malloc(sizeof(struct CSR_mtx));
-  read_matrix((char**)"../data/test2.mtx", mtx_coo_fmt, 0);
+  read_matrix((char**)"../data/test1.mtx", mtx_coo_fmt, 1);
   coo_to_csr(mtx_coo_fmt, mtx_csr_fmt);
   float time_taken;
   float num = triagle_counting_sequential_masked_implementation(mtx_csr_fmt,&time_taken,0);
-  EXPECT_EQ(12, num);
+  EXPECT_EQ(18, num);
 };
+
 
 int main(int argc, char** argv)
 {
